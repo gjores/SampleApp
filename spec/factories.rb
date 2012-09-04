4eb62aa7b14f1d12edbb76e4ac2a6397d2a5c2e4
Petter Gjöres <gjores@gmail.com>
2012-09-04 19:21:47 +0200

    fucked it all up

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
 	user.name 				   "Petter Gjores"
-	user.email				   "email@email.com"
+	user.email				   "gjores@gmail.com"
 	user.password              "foobar"
 	user.password_confirmation "foobar"
 end
