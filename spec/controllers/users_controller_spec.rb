647b4618af3df5e0a565e29b0c86b849953db291
Petter Gjöres <gjores@gmail.com>
2012-09-14 07:18:12 +0200

    Edit and update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -73,4 +73,72 @@ render_views
   	response.should have_selector('title', :content => "Sign up")
   end
 
+  describe "GET 'edit" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector('title', :content => "Edit user")
+    end
+
+    it "should hava a link to gravatar" do
+      get :edit, :id => @user
+      response.should have_selector('a', :href => 'http://gravatar.com/emails',
+                                         :content => "change")
+    end
+  end
+
+  describe "PUT 'update" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    describe "failure" do
+      before(:each) do
+      @attr = {:name => "", :email => "", :password => "", :password_confirmation => ""}
+    end
+      it "should rerender the same page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr      
+        response.should have_selector('title', :content => "Edit user")
+      end
+      
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = {  :name => "Shithead", :email => "shit@head.com", 
+                   :password => 'foobar123', :password_confirmation => "foobar123"  }
+      end
+    end
+        it "should change the user attr" do
+          put :update, :id => @user, :user => @attr
+          user = assigns(:user)
+          @user.reload
+          @user.name.should == user.name
+          @user.email.should == user.email
+          @user.encrypted_password.should == user.encrypted_password
+        end
+
+        it "should hava flash message" do
+          put :update, :id => @user, :user => @attr
+
+          flash[:success => "Profile updated."]
+          
+        end
+    
+  end
+
 end
