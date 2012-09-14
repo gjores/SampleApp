d6e0f27e8b27b11bb668b59ddde8beeaf53a4070
Petter Gjöres <gjores@gmail.com>
2012-09-14 16:01:50 +0200

    Friendlyforwarding anf correct user req

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,13 +21,28 @@ module SessionsHelper
 	def sign_out
 		cookies.delete(:remember_token)
 		current_user = nil
-		
+		clear_session_return_to
+	end
+	def clear_session_return_to
+		session[:return_to] = nil
+	end
+
+	def current_user?(user)
+		user == current_user
 	end
 
 	def deny_access
-      redirect_to signin_path, :notice => "Please sign in!" 
+	  store_location
+	  redirect_to signin_path, :notice => "Please sign in!" 
+    end
+
+    def store_location
+		session[:return_to] = request.fullpath
     end
     
+    def redirect_back_or(default)
+    	redirect_to(session[:return_to] || default )
+    end
 
 	private
 
